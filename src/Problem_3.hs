a99d2039face553c5660bd4c0ab0ef34c25c789b
박경준 <parkryan91@gmail.com>
2019-08-13 13:36:41 +0900

    solve problem 3

---
+++ src/Problem_3.hs
@@ -0,0 +1,13 @@
+module Problem_3
+    ( solve
+    ) where
+
+isqrt 0 = 0
+isqrt 1 = 1
+isqrt n = head $ dropWhile (\x -> x*x > n) $ iterate (\x -> (x + n `div` x) `div` 2) (n `div` 2)
+
+factorsOf xs = concat [[x,xs`div`x] | x <- [1..isqrt(xs)], xs `mod` x == 0]
+isPrime xs = length (factorsOf xs) == 2
+
+input = 600851475143
+solve = last (filter isPrime (factorsOf input))
